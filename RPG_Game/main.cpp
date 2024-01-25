ff857bc2ad14528ff409fc35eeb4ef0d9376eaa0
Aeonillluminate <nathan.a.math@gmail.com>
2024-01-25 00:10:04 -0500

    Went over functions and classes - final video watched is part 4.
    TO DO:
    Watch part 5 tomorrow and update main branch.

--- RPG_Game/main.cpp
+++ RPG_Game/main.cpp
@@ -1,14 +1,54 @@
 #include <iostream>
 
-void PrintHello()
+class Enemy
 {
-	std::cout << "Hello!" << std::endl;
-} 
+public:
+	int health = 0;
+	int damage = 0;
 
+	void kill()
+	{
+		std::cout << "I am killing" << std::endl;
+	}
+
+	void Heal()
+	{
+		health += 10;
+		std::cout << "My health is now " << health << std::endl;
+	}
+
+	void Walk()
+	{
+		std::cout << "I am walking!" << std::endl;
+	}
+};
 int main()
 {
 
-	PrintHello();
+	Enemy goblin;
+
+	goblin.health = 100;
+	goblin.damage = 50;
+	goblin.Walk();
+
+	std::cout << "Goblin stats are: " << std::endl;
+	std::cout << goblin.health << std::endl;
+	std::cout << goblin.damage << std::endl;
+
+	std::cout << goblin.health << std::endl;
+
+	goblin.Heal();
+
+	//--------------------------------------
+
+	Enemy dragon;
+	dragon.health = 200;
+	dragon.damage = 60;
+
+	std::cout << "Dragon stats are: " << std::endl;
+	std::cout << dragon.health << std::endl;
+	std::cout << dragon.damage << std::endl;
 
 	return 0;
 }
+
